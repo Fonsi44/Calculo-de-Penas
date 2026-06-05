ca63d25096bcf107506549dd9fdf48e302092b33
FonsiRoiget <alfons@RoigetGimenez.onmicrosoft.com>
2026-06-05 11:45:19 +0200

    chore: eliminar carpeta _archived_unused sin restaurar

--- _archived_unused/lib/api-helpers.ts
+++
@@ -1,7 +0,0 @@
-export function apiSuccess<T>(data: T, status = 200) {
-  return Response.json({ data }, { status });
-}
-
-export function apiError(message: string, status = 400) {
-  return Response.json({ error: message }, { status });
-}
