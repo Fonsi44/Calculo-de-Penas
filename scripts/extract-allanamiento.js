1d17990bd278a4024616a76c60405ed0e205dedc
fonsi <alfonsroiget@gmail.com>
2026-07-27 13:35:09 +0200

    chore(cleanup): remove obsolete scripts and one-off tooling

--- scripts/extract-allanamiento.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const posts = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'scripts', 'target_posts.json'), 'utf8'));
-const post = posts.find(p => p.slug === 'allanamiento-ilegal-violacion-domicilio-honduras');
-
-fs.writeFileSync(path.join(process.cwd(), 'scripts', 'allanamiento_original.html'), post.body);
-console.log('allanamiento_original.html created');
--- scripts/extract-prestaciones.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const posts = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'scripts', 'target_posts.json'), 'utf8'));
-const post = posts.find(p => p.slug === 'calcular-prestaciones-laborales-honduras');
-
-fs.writeFileSync(path.join(process.cwd(), 'scripts', 'prestaciones_original.html'), post.body);
-console.log('prestaciones_original.html created');
